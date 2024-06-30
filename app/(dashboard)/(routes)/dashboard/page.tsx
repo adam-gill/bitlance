f6e3f3f8dde66945052b2eba5d7304e6381505aa
adam-gill <adam.douglas.gill@gmail.com>
2024-06-30 16:29:28 -0400

    added example route: dashbaord and copied over Renox's last commit

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1 className='text-2xl text-primaryBitlanceLightGreen bg-primaryBitlanceDark'>This is an example page with the route of /dashboard</h1>
+        </div>
+    );
+};
+
+export default Dashboard;

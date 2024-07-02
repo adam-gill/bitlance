067d8c96d620d62514d98aeb74d649c4f4f84c25
RonexLemon <ronexondimu@gmail.com>
2024-07-02 16:41:39 +0300

    change font

--- components/pages/auth/login.tsx
+++ components/pages/auth/login.tsx
@@ -17,7 +17,7 @@ const LoginPage =()=>{
         <div className="bg-primaryBitlanceDark h-3/4 w-3/4 rounded-3xl grid grid-cols-2 text-white">
 <div className="flex justify-center mt-10">
         <Card className="w-3/4 h-5/6 bg-transparent border-none   ">
-                <CardHeader className="flex justify-center items-center"><span className="text-6xl text-white">Welcome</span></CardHeader>
+                <CardHeader className="flex justify-center items-center"><span className="text-2xl text-white">WELCOME</span></CardHeader>
                 <CardDescription className="flex justify-center items-center pb-4" >We are glad to see you back with us</CardDescription>
                 <CardContent className="flex  flex-col gap-4 justify-between items-center w-full">
                         <Input   className="w-3/4 " placeholder="Email"/>

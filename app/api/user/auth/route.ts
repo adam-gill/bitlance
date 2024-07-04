59c8ed19ba52245fba4f7063ac153a553a279a83
adam-gill <adam.douglas.gill@gmail.com>
2024-07-04 15:30:31 -0400

    add prisma generate to package.json scripts

--- app/api/user/auth/route.ts
+++ app/api/user/auth/route.ts
@@ -21,7 +21,7 @@ export async function POST(request: NextRequest) {
     // Find the user in the database
     const user = await prisma.user.findUnique({
       where: {
-        email:email
+        email: email,
       },
     });
     console.log(user)

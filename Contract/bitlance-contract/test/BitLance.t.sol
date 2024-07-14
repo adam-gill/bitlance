59f17ce8b7e3308a06c7842a780581d487d47879
RonexLemon <ronexondimu@gmail.com>
2024-07-14 12:55:41 +0300

    check is token is allowed

--- Contract/bitlance-contract/test/BitLance.t.sol
+++ Contract/bitlance-contract/test/BitLance.t.sol
@@ -7,15 +7,38 @@ import {BITLANCE}  from "../src/BITLANCE.sol";
 
 contract BITLANCETEST is Test{
 BITLANCE public bitlance;
+address public tokenA = address(1);
+address public tokenB = address(2);
+//  function addToken(address _addr)public onlyManager{
+//         allowedTokens[_addr] = true;
+
+//     }
 
 function setUp()public{
     bitlance = new BITLANCE();
+    vm.label(tokenA, "tokenA");
+    vm.label(tokenB, "tokenB");
+    bitlance.addToken(tokenA);
+
+   
 
 }
 
-function test_true()public{
-     assertEq(true, true);
+/****
+ * @dev BITLANCEMANAGEMENT CONTRACT
+ * 1. check if an address token is allowed
+ * 2. check if an address is not allowed
+ */
+function test_checkIfTokenIsAllowed()public view{
+   bool isAllowed =  bitlance.allowedTokens(tokenA);
+     assertEq(isAllowed, true);
 }
+
+function test_checkIfTokenIsNotAllowed()public view{
+   bool isAllowed =  bitlance.allowedTokens(tokenB);
+     assertEq(isAllowed, false);
+}
+
 
 
 }

8b02b2c8792997bec5dac75acec055aa63cb3d44
RonexLemon <ronexondimu@gmail.com>
2024-07-14 15:38:55 +0300

    request job

--- Contract/bitlance-contract/test/BitLance.t.sol
+++ Contract/bitlance-contract/test/BitLance.t.sol
@@ -2,77 +2,124 @@
 pragma solidity ^0.8.25;
 
 import {Test, console} from "forge-std/Test.sol";
-
-import {BITLANCE}  from "../src/BITLANCE.sol";
+import {BITLANCE} from "../src/BITLANCE.sol";
 import {USDCMOCK} from "../src/ERC20MOCK.sol";
 
-contract BITLANCETEST is Test{
-BITLANCE public bitlance;
-USDCMOCK public usdc;
-address public tokenA = address(1);
-address public tokenB = address(2);
-address public manager1= address(3);
+contract BITLANCETEST is Test {
+    BITLANCE public bitlance;
+    USDCMOCK public usdc;
+    string[] public jobIds = ["12345", "54321"];
+    address public tokenA = address(1);
+    address public tokenB = address(2);
+    address public manager1 = address(3);
+    address public client = address(4);
+    address public freelancer = address(5);
+    uint256 public jobAmount = 10 * 10 ** 18;
+    event RequestJob(address indexed _freelancer,string indexed _jobid);
+    event InitJob(address indexed  client,address indexed  freelancer,string jobid,uint256 _amout);
 
-//  function addToken(address _addr)public onlyManager{
-//         allowedTokens[_addr] = true;
+    // struct Job {
+    //     string jobid;
+    //     uint256 amount;
+    //     address stableCoinToken;
+    //     address[] freelancers;
+    //     address selectedFreelancer;
+    //     address client;
+    //     bool isStarted;
+    //     bool isPaid;
+    //     bool hasConflict;
+    // }
 
-//     }
+    function setUp() public {
+        bitlance = new BITLANCE();
+        usdc = new USDCMOCK();
 
-function setUp()public{
-    bitlance = new BITLANCE();
-    usdc = new USDCMOCK();
-    
-    vm.label(tokenA, "tokenA");
-    vm.label(tokenB, "tokenB");
-    bitlance.addToken(address(usdc));
-    usdc.mint(tokenA,1000);
-    
-
-   
-
-}
-
-/****
- * @dev BITLANCEMANAGEMENT CONTRACT
- * 1. check if an address token is allowed
- * 2. check if an address is not allowed
- * 3. check if the call is a manager
- * 4. check an address not  manager
- * 5. add another address to the manager list
- * 6. check user balance
- */
-function test_checkIfTokenIsAllowed()public view{
-   bool isAllowed =  bitlance.allowedTokens(address(usdc));
-     assertEq(isAllowed, true);
-}
-
-function test_checkIfTokenIsNotAllowed()public view{
-   bool isAllowed =  bitlance.allowedTokens(tokenB);
-     assertEq(isAllowed, false);
-}
-
-//the address(this) was the deployer of the contract -> automatic added to the list of the managers
-function test_isManager()public view{
-    bool isManager = bitlance.isManager(address(this));
-    assertEq(isManager, true);
-}
-
-function test_notManager()public view{
-    bool isManager = bitlance.isManager(manager1);
-    assertEq(isManager, false);
-}
-function test_addNewManager2()public {
-    bitlance.addManager(manager1);
-    bool isManager = bitlance.isManager(manager1);
-    assertEq(isManager, true);
+        vm.label(tokenA, "tokenA");
+        vm.label(tokenB, "tokenB");
+        bitlance.addToken(address(usdc));
+        usdc.mint(freelancer,1000);
     }
 
+    /****
+     * @dev BITLANCEMANAGEMENT CONTRACT
+     * 1. check if an address token is allowed
+     * 2. check if an address is not allowed
+     * 3. check if the call is a manager
+     * 4. check an address not a manager
+     * 5. add another address to the manager list
+     * 6. check user balance
+     */
+    function test_checkIfTokenIsAllowed() public view {
+        bool isAllowed = bitlance.allowedTokens(address(usdc));
+        assertEq(isAllowed, true);
+    }
 
-function test_usdcBalance()public view{
-    uint256 balance = usdc.balanceOf(tokenA);
-    assertEq(balance, 1000*10**18);
-}
+    function test_checkIfTokenIsNotAllowed() public view {
+        bool isAllowed = bitlance.allowedTokens(tokenB);
+        assertEq(isAllowed, false);
+    }
 
+    // the address(this) was the deployer of the contract -> automatic added to the list of the managers
+    function test_isManager() public view {
+        bool isManager = bitlance.isManager(address(this));
+        assertEq(isManager, true);
+    }
+
+    function test_notManager() public view {
+        bool isManager = bitlance.isManager(manager1);
+        assertEq(isManager, false);
+    }
+
+    function test_addNewManager() public {
+        bitlance.addManager(manager1);
+        bool isManager = bitlance.isManager(manager1);
+        assertEq(isManager, true);
+    }
+
+    function test_usdcBalance() public view {
+        uint256 balance = usdc.balanceOf(freelancer);
+        assertEq(balance, 1000 * 10 ** 18);
+    }
+
+    /****
+     * @dev BITLANCE CONTRACT
+     * 1. request for job
+     * 2. init job
+     * 3. release payment
+     * 4. raise an issue
+     * 5. refund back
+     */
+
+    function test_jobRequest() public {
+        // Set the message sender to be the client for this transaction
+        string memory jobId = jobIds[1];
+        vm.startPrank(freelancer);
+       
+        
+       console.logAddress(freelancer);
+        // Approve the BITLANCE contract to spend USDC on behalf of the client
+        // usdc.approve(address(bitlance), jobAmount);
+        
+        // Request a job
+        
+        
+        bitlance.requestJob(jobId, client, jobAmount);
+        // Check that the job request was successful
+      (uint256 _amount,,,address _client,bool isStarted,bool isPaid,bool hasConflict) = bitlance.jobs(jobId);
+      
+      assertEq(_amount, jobAmount);
+      assertEq(_client, client);
+      assertEq(isStarted, false);
+      assertEq(isPaid, false);
+      assertEq(hasConflict, false);
+    vm.stopPrank();
+        
+        
+        
+
+       
+        
+    }
 
 
 }

0c6c5b4ac1ec0513c9debd948ef8e3a09f0d2c4b
RonexLemon <ronexondimu@gmail.com>
2024-07-15 19:57:33 +0300

    types

---
+++ types/data-types.ts
@@ -0,0 +1,12 @@
+export interface FreelancerData {
+    bio: string;
+    skills: string;
+    portfolio_link: string;
+    social_link: string;
+}
+
+export interface ClientData {
+    company_name: string;
+    company_description: string;
+    websiteLink: string;
+}

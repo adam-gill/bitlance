db19ab1b6d4d13c9eecf2f07e3684f2bb8828b9d
Sarah Umoh <156671954+gorgeoussarah@users.noreply.github.com>
2024-07-15 18:53:59 +0530

    Update libCheckAddress.sol
    I added documentation just above the function stating the params and what the function does.

--- Contract/bitlance-contract/src/libCheckAddress.sol
+++ Contract/bitlance-contract/src/libCheckAddress.sol
@@ -3,6 +3,13 @@ pragma solidity ^0.8.25;
 
 library ArrayForAddress {
 
+ /**
+     * Checks if a specific address is present in an array of addresses.
+     * freelancers: The array of addresses to search.
+     * _freelancer: The address being examined to determine if it exists within the array.
+     * Returns true if the address is found in the array, otherwise returns false.
+     */
+
     function isAddressAvailable(address[] memory freelancers, address _freelancer) public pure returns (bool) {
         for (uint256 i = 0; i < freelancers.length; i++) {
             if (freelancers[i] == _freelancer) {

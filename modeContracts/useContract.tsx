3a20ba743b82f02ea088e822a39fbb44a4c757dd
RonexLemon <ronexondimu@gmail.com>
2024-07-17 00:48:49 +0300

    bitlance contract endpoints

---
+++ modeContracts/useContract.tsx
@@ -0,0 +1,107 @@
+import React from "react";
+
+
+import { useWriteContract,useReadContract } from "wagmi";
+import BITLANCEABI from "../abi/bitlance.json"
+import { BITLANCECONTRACT } from "@/constant/contracts";
+
+
+
+const useContract =()=>{
+    const {writeContractAsync:bitlanceContract} = useWriteContract()
+
+    //request job
+    // string memory _jobid,address client,uint256 amount
+    const RequestJob = async(userAddress:string,clientAddress:string,amount:bigint) =>{
+        const tx = await bitlanceContract({
+            address:BITLANCECONTRACT,
+            abi:BITLANCEABI.abi,
+            functionName:"requestJob",
+            args:[userAddress,clientAddress,amount]
+        })
+        return tx
+            
+    }
+    
+    //initi job
+    //string memory _jobid,address freelancer,address stabletoken
+
+    const InitJob = async(jobId:string,freelancerAddress:string,token:string)=>{
+        const tx = await bitlanceContract({
+            address:BITLANCECONTRACT,
+            abi:BITLANCEABI.abi,
+            functionName:"initializeJob",
+            args:[jobId,freelancerAddress,token]
+            })
+            return tx
+    }
+
+    //release payment
+    //takes jobid
+    const ReleasePayment = async(jobId:string)=>{
+        const tx = await bitlanceContract({
+            address:BITLANCECONTRACT,
+            abi:BITLANCEABI.abi,
+            functionName:"releasePayament",
+            args:[jobId]
+            })
+            return tx
+    }
+
+
+    //raise a dispute
+    //takes jobid
+    const RaiseDispute = async(jobId:string)=>{
+        const tx = await bitlanceContract({
+            address:BITLANCECONTRACT,
+            abi:BITLANCEABI.abi,
+            functionName:"raiseConflict",
+            args:[jobId]
+            })
+            return tx
+
+    }
+
+
+
+    //solve a dispute (refund)
+    //takes jobid and address
+    const SolveDispute = async(jobId:string,address:string)=>{
+        const tx = await bitlanceContract({
+            address:BITLANCECONTRACT,
+            abi:BITLANCEABI.abi,
+            functionName:"refund",
+            args:[jobId,address]
+            })
+            return tx}
+
+
+    //allow tokens
+    //takes token address
+    const AddTokens = async(token:string)=>{
+        const tx = await bitlanceContract({
+            address:BITLANCECONTRACT,
+            abi:BITLANCEABI.abi,
+            functionName:"addToken",
+            args:[token]
+            })
+            return tx
+            }
+//add manager
+//takes address
+const AddManager = async(address:string)=>{
+    const tx = await bitlanceContract({
+        address:BITLANCECONTRACT,
+        abi:BITLANCEABI.abi,
+        functionName:"addManager",
+        args:[address]
+        })
+        return tx
+        }
+
+
+
+    return{AddManager,AddTokens,RaiseDispute,RequestJob,ReleasePayment,SolveDispute,InitJob}
+}
+
+export default useContract;

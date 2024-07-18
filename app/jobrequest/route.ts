618b5c2ca51c3d0b0472f5a57696b98d8d9b86b5
RonexLemon <ronexondimu@gmail.com>
2024-07-18 10:03:08 +0300

    freelancers end point

---
+++ app/jobrequest/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+type Data = {
+  job_id: string;
+  freelancer_id: string;
+  client_id: string;
+  freelancer_address: string;
+};
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+
+    const { job_id, freelancer_id, client_id, freelancer_address }: Data = body;
+
+    // Validate the request data
+    if (!job_id || !freelancer_id || !client_id || !freelancer_address) {
+      return NextResponse.json({ error: 'All fields are required' }, { status: 400 });
+    }
+
+    const jobFreelancer = await prisma.jobFreelancer.create({
+      data: {
+        job_id,
+        freelancer_id,
+        client_id,
+        freelancer_address,
+      },
+    });
+
+    return NextResponse.json({ success: true, jobFreelancer }, { status: 201 });
+  } catch (error) {
+    console.error("Error creating JobFreelancer:", error);
+    return NextResponse.json({ success: false, message: "Internal server error" }, { status: 500 });
+  }
+}
